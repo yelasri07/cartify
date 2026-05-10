6704a2c6afef2f9c9fba5e426ef9b49e2f5a9369
aelmir || Sphinex0 <adnane.elmir1@gmail.com>
2026-05-10 18:08:26 +0100

    aaa

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,18 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    {
-        path: "auth",
-        loadComponent: () => import('./features/auth/auth.component').then(m => m.AuthComponent),
-        loadChildren: () => import('./features/auth/auth.routes').then(m => m.routes)
-    }
+  {
+    path: 'auth',
+    loadComponent: () =>
+      import('./features/auth/auth.component').then((m) => m.AuthComponent),
+    loadChildren: () =>
+      import('./features/auth/auth.routes').then((m) => m.routes),
+  },
+  {
+    path: '',
+    loadComponent: () =>
+      import('./layout/home-layout/home-layout.component').then((m) => m.HomeLayoutComponent),
+    loadChildren: () =>
+      import('./features/auth/auth.routes').then((m) => m.routes),
+  },
 ];
---
+++ frontend/src/app/layout/home-layout/home-layout.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from "../components/header/header.component";
+import { FooterComponent } from "../components/footer/footer.component";
+
+@Component({
+  selector: 'app-home-layout',
+  imports: [HeaderComponent, FooterComponent],
+  templateUrl: './home-layout.component.html',
+  styleUrl: './home-layout.component.scss'
+})
+export class HomeLayoutComponent {
+
+}
