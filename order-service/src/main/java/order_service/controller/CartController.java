8a553555233ee89183acf6c0cecfe3b77996a1ab
yelasri <elasriyoussef604@gmail.com>
2026-07-14 12:33:46 +0100

    connect mongodb with order service

--- order-service/src/main/java/order_service/controller/CartController.java
+++ order-service/src/main/java/order_service/controller/CartController.java
@@ -3,8 +3,12 @@ package order_service.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.RequiredArgsConstructor;
+
 @RestController
 @RequestMapping("/cart")
+@RequiredArgsConstructor
 public class CartController {
-    
+
+
 }
