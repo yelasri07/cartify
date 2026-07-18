2f9da5059f51e9e9b2112f67b06eb4ab9d51395c
yelasri <elasriyoussef604@gmail.com>
2026-07-18 22:43:04 +0100

    kafka testing

---
+++ order-service/src/main/java/order_service/kafka/OrderProducer.java
@@ -0,0 +1,17 @@
+package order_service.kafka;
+
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class OrderProducer {
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public void sendMessage(String topic, String message) {
+        System.out.println("aaaaaaaaaaaaaaaaa");
+        kafkaTemplate.send(topic, message);
+    }
+}
