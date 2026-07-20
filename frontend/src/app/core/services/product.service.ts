77c7d2f1ca76fbe748c725a09ea46e2fd6902b95
aelmir || Sphinex0 <adnane.elmir1@gmail.com>
2026-07-20 10:34:43 +0100

    fix(order): fix search with pagination

--- frontend/src/app/core/services/product.service.ts
+++ frontend/src/app/core/services/product.service.ts
@@ -13,6 +13,8 @@ export class ProductService {
   private readonly _page = signal(0);
   private readonly _products = signal<Product[]>([]);
   private readonly _loading = signal(false);
+  searchValue = signal('');
+  sortBy = signal('id');
 
   // Exposed signals
   readonly page = computed(() => this._page());
@@ -21,89 +23,105 @@ export class ProductService {
   readonly isFirstPage = computed(() => this._page() === 0);
   readonly isLastPage = computed(() => this._products().length < this.pageSize);
 
-  loadProducts(userId?: string, searchValue?: string, sortedBy?: string) {
+  loadProducts(userId?: string) {
     this._loading.set(true);
     const params = new HttpParams()
       .set('page', this._page().toString())
       .set('size', this.pageSize.toString())
-      .set('search', searchValue ? searchValue : "")
-      .set('sortedBy', sortedBy ? sortedBy : "id")
+      .set('search', this.searchValue())
+      .set('sortedBy', this.sortBy());
 
-    this.http.get<Product[]>(!userId ? API.GET_POSTS : `${API.GET_PROFILE_POSTS}/${userId}`, { params }).subscribe({
-      next: (products) => {
-        this._products.set(products);
-        this._loading.set(false);
-      },
-      error: (err) => {
-        this._loading.set(false);
-        throw err
-      }
-    });
+    this.http
+      .get<
+        Product[]
+      >(!userId ? API.GET_POSTS : `${API.GET_PROFILE_POSTS}/${userId}`, { params })
+      .subscribe({
+        next: (products) => {
+          this._products.set(products);
+          this._loading.set(false);
+        },
+        error: (err) => {
+          this._loading.set(false);
+          throw err;
+        },
+      });
   }
 
   nextPage(userId?: string) {
     if (!this.isLastPage()) {
-      this._page.update(p => p + 1);
+      this._page.update((p) => p + 1);
       this.loadProducts(userId);
     }
   }
 
   previousPage(userId?: string) {
     if (!this.isFirstPage()) {
-      this._page.update(p => Math.max(0, p - 1));
+      this._page.update((p) => Math.max(0, p - 1));
       this.loadProducts(userId);
     }
   }
 
   productUnshift(product: Product) {
     if (!this.isFirstPage()) return;
-    this._products.set(this._products().filter(p => p.id !== product.id))
-    this._products.update(p => [product, ...(p.length === this.pageSize ? p.splice(0, p.length - 1) : p)])
+    this._products.set(this._products().filter((p) => p.id !== product.id));
+    this._products.update((p) => [
+      product,
+      ...(p.length === this.pageSize ? p.splice(0, p.length - 1) : p),
+    ]);
   }
 
   productDelete(productId: string) {
     const deletedProduct: Product = {
       id: productId,
-      name: "",
-      description: "",
+      name: '',
+      description: '',
       price: 0,
       quantity: 0,
-      user_id: ""
-    }
+      user_id: '',
+    };
 
-    this._products.set(this._products().map(p => {
-      if (p.id === productId) {
-        return deletedProduct;
-      }
+    this._products.set(
+      this._products().map((p) => {
+        if (p.id === productId) {
+          return deletedProduct;
+        }
 
-      return p
-    }))
+        return p;
+      }),
+    );
   }
 
   productUpdate(product: Product) {
-    this._products.set(this._products().map(p => {
-      if (p.id === product.id) {
-        return product;
-      }
+    this._products.set(
+      this._products().map((p) => {
+        if (p.id === product.id) {
+          return product;
+        }
 
-      return p
-    }))
-
+        return p;
+      }),
+    );
   }
 
   resetPage() {
-    this._page.set(0)
+    this._page.set(0);
   }
 
   resetProducts() {
-    this._products.set([])
+    this._products.set([]);
+    this.searchValue.set('');
+    this.sortBy.set('id');
   }
 
   submitProduct(product: any, productId?: string) {
-    return !productId ? this.http.post<Product>(API.CREATE_PRODUCT, product) : this.http.put<Product>(API.UPDATE_PRODUCT + '/' + productId, product)
+    return !productId
+      ? this.http.post<Product>(API.CREATE_PRODUCT, product)
+      : this.http.put<Product>(API.UPDATE_PRODUCT + '/' + productId, product);
   }
 
   submitDeleteProduct(productId: string) {
-    return this.http.delete<{ productId: string, message: string }>(API.DELETE_PRODUCT + '/' + productId)
+    return this.http.delete<{ productId: string; message: string }>(
+      API.DELETE_PRODUCT + '/' + productId,
+    );
   }
 }
